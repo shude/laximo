ab1f83d4d149693d32ffad1235ec6f7fee93c387
bek <isakhmet.bek@cbc-group.kz>
2020-09-04 15:06:50 +0600

    Upgrade

--- Laximo.php
+++ Laximo.php
@@ -5,6 +5,9 @@ namespace shude\Laximo;
 
 
 use shude\Laximo\Client\SoapClientInterface;
+use shude\Laximo\Objects\AftermarketDetailObject;
+use shude\Laximo\Objects\AftermarketDetailsListObject;
+use shude\Laximo\Objects\AftermarketReplacementsListObject;
 use shude\Laximo\Objects\CatalogListObject;
 use shude\Laximo\Objects\CatalogObject;
 use shude\Laximo\Objects\CategoryListObject;
@@ -12,10 +15,15 @@ use shude\Laximo\Objects\DetailListObject;
 use shude\Laximo\Objects\DetailReferencesListObject;
 use shude\Laximo\Objects\FilterObject;
 use shude\Laximo\Objects\ImageMapObject;
+use shude\Laximo\Objects\ListQuickDetailObject;
+use shude\Laximo\Objects\ListQuickGroupObject;
 use shude\Laximo\Objects\UnitListObject;
 use shude\Laximo\Objects\UnitObject;
 use shude\Laximo\Objects\VehicleListObject;
 use shude\Laximo\Objects\VehicleObject;
+use shude\Laximo\Objects\WizardNextStep;
+use shude\Laximo\Objects\WizardObject;
+use shude\Laximo\Objects\WizardStepObject;
 use shude\Laximo\Query\Query;
 use shude\Laximo\Query\QueryBuilderInterface;
 
@@ -40,6 +48,55 @@ class Laximo
         );
     }
 
+    public function addFindOem(string $oem, $options, $brand = '', $replacementTypes = '') : self
+    {
+        $this->addQuery('FindOEM',[
+            'Locale' => $this->locale,
+            'OEM' => $oem,
+            'Options' => $options,
+            'Brand' => $brand,
+            'ReplacementTypes' => $replacementTypes
+        ]);
+
+        $this->resultObjectStack[] = AftermarketDetailsListObject::class;
+        return $this;
+    }
+
+    public function addFindDetail(int $detailId, $options) : self
+    {
+        $this->addQuery('FindDetail',[
+            'Locale' => $this->locale,
+            'DetailId' => $detailId,
+            'Options' => $options
+        ]);
+
+        $this->resultObjectStack[] = AftermarketDetailsListObject::class;
+        return $this;
+    }
+
+    public function addFindOEMCorrection(string $oem, $brand = '') : self
+    {
+        $this->addQuery('FindOEMCorrection',[
+            'Locale' => $this->locale,
+            'Brand' => $brand,
+            'OEM' => $oem,
+        ]);
+
+        $this->resultObjectStack[] = AftermarketDetailsListObject::class;
+        return $this;
+    }
+
+    public function addFindReplacements(int $detailId) : self
+    {
+        $this->addQuery('FindReplacements',[
+            'Locale' => $this->locale,
+            'DetailId' => $detailId,
+        ]);
+
+        $this->resultObjectStack[] = AftermarketReplacementsListObject::class;
+        return $this;
+    }
+
     public function addListCatalogs() : self
     {
         $this->addQuery('ListCatalogs',[
@@ -59,6 +116,45 @@ class Laximo
         ]);
 
         $this->resultObjectStack[] = CatalogObject::class;
+
+        return $this;
+    }
+
+    public function addGetWizard2(string $catalog, string $ssd = '') : self
+    {
+        $this->addQuery('GetWizard2',[
+            'Locale' => $this->locale,
+            'Catalog' => $catalog,
+            'ssd' => $ssd
+        ]);
+
+        $this->resultObjectStack[] = WizardObject::class;
+
+        return $this;
+    }
+
+    public function addFindVehicleByWizard2(string $catalog, string $ssd) : self
+    {
+        $this->addQuery('FindVehicleByWizard2',[
+            'Locale' => $this->locale,
+            'Catalog' => $catalog,
+            'ssd' => $ssd
+        ]);
+
+        $this->resultObjectStack[] = VehicleListObject::class;
+
+        return $this;
+    }
+
+    public function addGetWizardNextStep2($catalog, $ssd = '')
+    {
+        $this->addQuery('GetWizardNextStep2',[
+            'Locale' => $this->locale,
+            'Catalog' => $catalog,
+            'ssd' => $ssd
+        ]);
+
+        $this->resultObjectStack[] = WizardNextStep::class;
 
         return $this;
     }
@@ -249,6 +345,34 @@ class Laximo
         ]);
 
         $this->resultObjectStack[] = DetailReferencesListObject::class;
+    }
+
+    public function addListQuickGroups($catalog, $vehicleId, $ssd) : self
+    {
+        $this->addQuery('ListQuickGroup',[
+            'Locale' => $this->locale,
+            'Catalog' => $catalog,
+            'VehicleId' => $vehicleId,
+            'ssd' => $ssd
+        ]);
+
+        $this->resultObjectStack[] = ListQuickGroupObject::class;
+        return $this;
+    }
+
+    public function addListQuickDetails($catalog, $vehicleId, $ssd, $quickGroupId) : self
+    {
+        $this->addQuery('ListQuickDetail',[
+            'Locale' => $this->locale,
+            'Catalog' => $catalog,
+            'VehicleId' => $vehicleId,
+            'QuickGroupId' => $quickGroupId,
+            'All' => 1,
+            'ssd' => $ssd
+        ]);
+
+        $this->resultObjectStack[] = ListQuickDetailObject::class;
+        return $this;
     }
 
     public function execute()
